6dfc616b1c5e31b914f278bb2df07bdfb0a2ca53
Furkan Sahin <aliince.000000@gmail.com>
2024-08-07 09:22:52 +0300

    Updated - 07.08.2024

--- notebook/lib/note_content_sections/content_note_section.dart
+++ notebook/lib/note_content_sections/content_note_section.dart
@@ -1,21 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ContentNoteSection extends StatefulWidget {
-  const ContentNoteSection({super.key});
+  final void Function(String) updateContent;
+  final String? content;
+  const ContentNoteSection(
+      {super.key, required this.updateContent, required this.content});
 
   @override
   State<ContentNoteSection> createState() => _ContentNoteSectionState();
 }
 
 class _ContentNoteSectionState extends State<ContentNoteSection> {
+  late TextEditingController _contentcontroller;
+  bool _isEditing = false;
+  final FocusNode _focusNode = FocusNode();
 
+  @override
+  void initState() {
+    super.initState();
+    _contentcontroller = TextEditingController(text: widget.content);
+  }
 
+  @override
+  void dispose() {
+    _contentcontroller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: SizedBox(),
+      onTap: () {
+        setState(() {
+          _isEditing = true;
+          _focusNode.requestFocus();
+        });
+      },
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: _isEditing
+              ? TextField(
+                  controller: _contentcontroller,
+                  focusNode: _focusNode,
+                  maxLines: null,
+                  expands: true,
+                  onSubmitted: (value) {
+                    setState(() {
+                      _isEditing = false;
+                    });
+                  },
+                  onChanged: (content) {
+                    widget.updateContent(content);
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Notunuzu girin',
+                  ),
+                )
+              : Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 9),
+                  child: Text(
+                    _contentcontroller.text.isNotEmpty
+                        ? _contentcontroller.text
+                        : 'Not içeriği',
+                    style: _contentcontroller.text.isNotEmpty
+                        ? const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black87,
+                          )
+                        : const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black45,
+                          ),
+                  ),
+                ),
+        ),
+      ),
     );
   }
 }
